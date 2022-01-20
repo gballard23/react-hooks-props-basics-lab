6a27e9a27926d252730ddbf3f904383ba980253f
Gerren <g.ballard23@outlook.com>
2022-01-20 17:18:49 -0600

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Links from "./Links"
 
-function About(props) {
-  
-  
+
+function About(props) {  
   
   return (
     <div id="about">
       <h2>About Me</h2>
-      {if(props.bio < 0 ){
-   return null;
-  }else{
-     return( <p>{props.bio}</p>)
-  }}
+
+     {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {<Links links={props.links} />}
+      <Links 
+      github={props.links.github}
+      linkedin={props.links.linkedin} 
+      />
     </div>
    );
   
--- src/components/Links.js
+++ src/components/Links.js
@@ -4,8 +4,8 @@ function Links(props) {
     return (
      <div>
         <h3>Links</h3>
-        <a href={props.links.github}>{props.links.github}</a>
-        <a href={props.links.linkedin}>{props.links.linkedin}</a>
+        <a href={props.github}>{props.github}</a>
+        <a href={props.linkedin}>{props.linkedin}</a>
      </div>
     );
     
